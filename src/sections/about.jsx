d88a60d5c0f7962c98f3e8b4d1df09ffc5961a9e
Papop Lekhapanyaporn <75293379+Retaehc-pop@users.noreply.github.com>
2021-10-20 02:42:35 +0700

    update home

--- src/sections/about.jsx
+++ src/sections/about.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import AwesomeSlider from 'react-awesome-slider';
-// import AwsSliderStyles from 'react-awesome-slider/src/styles';
 import withAutoplay from 'react-awesome-slider/dist/autoplay';
 import 'react-awesome-slider/dist/styles.css';
 
@@ -15,11 +14,9 @@ function About () {
         bullets={false}
         // fillParent={true}
       >
-        <div data-src="img\C.png" />
-        <div data-src="img\Profile.jpg" />
-        <div data-src="img\python.jpg" />
-        <div data-src="img\Cplus.jpg" />
-        <div data-src="img\dbd.jpg" />
+        <div data-src="img\Artboard3.png" />
+        <div data-src="img\Artboard2.png" />
+        <div data-src="img\Artboard1.png" />
       </AutoplaySlider>
   </div>;
     
--- src/sections/skill.jsx
+++ src/sections/skill.jsx
@@ -219,17 +219,7 @@ function Skill(){
                               aria-labelledby="modal-modal-title"
                               aria-describedby="modal-modal-description"
                             >
-                              <Box sx={{position: 'absolute',
-                                        top: '50%',
-                                        left: '50%',
-                                        transform: 'translate(-50%, -50%)',
-                                        width: 400,
-                                        bgcolor: 'background.paper',
-                                        border: '2px solid #000',
-                                        boxShadow: 24,
-                                        p: 4,}}>
                                 <OD></OD>
-                              </Box>
                             </Modal>
                         </div> 
                         {console.log(open)}
@@ -335,7 +325,7 @@ function Skill(){
                     <Content>
                         <div data-aos='fade-up' className='codecard'>
                             <Card sx={{ maxWidth: 345 }}>
-                                <CardActionArea>
+                                <CardActionArea href='#'>
                                     <CardMedia
                                     component="img"
                                     height="140"
