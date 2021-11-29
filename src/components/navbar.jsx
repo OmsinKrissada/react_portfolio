3a77b42dc155ceca5e2bfb23b8f8d96889a73182
Krissada Singhakachain <46844213+OmsinKrissada@users.noreply.github.com>
2021-11-30 01:15:01 +0700

    attempt to animate nav bar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -10,32 +10,34 @@ function NavBar() {
 	const [isOpen, setisOpen] = useState(false);
 
 	return (
-		<div className={isOpen ? "navbar" : "navbar hidden"}>
+		<div className="navbar">
 			<img className="ham" src="/hamburger.png" alt="" onClick={() => setisOpen(!isOpen)} />
 			{/* <img class="ham" src="/hamburger.png" alt="" /> */}
-			<Link to="/">
-				<img href="#" src="/logo192.png" alt="Retaehc logo" width="50" height="50" />
-			</Link>
-			<p>
-				<Link className="na" to="about" style={style}>
-					ABOUT
+			<div className={isOpen ? "navbox" : "hidden navbox"}>
+				<Link to="/">
+					<img href="#" src="/logo192.png" alt="Retaehc logo" width="50" height="50" />
 				</Link>
-			</p>
-			<p>
-				<Link className="na" to="skills" style={style}>
-					SKILL
-				</Link>
-			</p>
-			<p>
-				<Link className="na" to="project" style={style}>
-					PROJECT
-				</Link>
-			</p>
-			<p>
-				<Link className="na" to="milestone" style={style}>
-					MILESTONE
-				</Link>
-			</p>
+				<p>
+					<Link className="na" to="about" style={style}>
+						ABOUT
+					</Link>
+				</p>
+				<p>
+					<Link className="na" to="skills" style={style}>
+						SKILL
+					</Link>
+				</p>
+				<p>
+					<Link className="na" to="project" style={style}>
+						PROJECT
+					</Link>
+				</p>
+				<p>
+					<Link className="na" to="milestone" style={style}>
+						MILESTONE
+					</Link>
+				</p>
+			</div>
 		</div>
 	);
 }
